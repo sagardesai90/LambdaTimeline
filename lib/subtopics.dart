b7379aca8548864a3383543e11b6d9d8ecbbbb32
sagardesai90 <saagardesai@gmail.com>
2019-01-04 11:19:29 -0600

    more subtopics

--- lib/subtopics.dart
+++ lib/subtopics.dart
@@ -24,4 +24,22 @@ List<Subtopic> subtopics = [
     duration: "1 week",
     toolkit: "https://learn.lambdaschool.com/fsw/module/recl0iyzs2vl89lza"
   ),
+  const Subtopic(
+    id: "3",
+    name: "User Interface II",
+    duration: "1 week",
+    toolkit: "https://learn.lambdaschool.com/fsw/module/recgvxywt6avgtmhr"
+  ),
+  const Subtopic(
+    id: "4",
+    name: "User Interface III",
+    duration: "1 week",
+    toolkit: "https://learn.lambdaschool.com/fsw/module/recavbbzhh8btymdm"
+  ),
+  const Subtopic(
+    id: "5",
+    name: "Git for Web Development",
+    duration: "1 week",
+    toolkit: "https://learn.lambdaschool.com/fsw/module/rect59e95n6osvocd"
+  ),
 ];
